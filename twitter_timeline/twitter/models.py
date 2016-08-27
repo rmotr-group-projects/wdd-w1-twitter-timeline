20337ef9a207b8cae616544d327d4804e859e5c0
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-27 18:52:31 +0200

    model migrations working

--- twitter_timeline/twitter/models.py
+++ twitter_timeline/twitter/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.conf import settings
 
 
 class Tweet(models.Model):
@@ -7,4 +10,11 @@ class Tweet(models.Model):
     #     "content": "hello world!",
     #     "created": "2016-08-26T10:03:28"
     # }
-    tweet_info_json = models.CharField(max_length=1500)
+    # tweet_info_json = models.CharField(max_length=1500)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
+    content = models.CharField(max_length=140, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class User(AbstractUser):
+    pass
