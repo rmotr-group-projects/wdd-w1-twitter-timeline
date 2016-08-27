0e90630c8603d14b692a0cafe86cd2862cd49e87
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-27 19:37:21 +0200

    urls and views

--- twitter_timeline/twitter/views.py
+++ twitter_timeline/twitter/views.py
@@ -2,10 +2,51 @@ from django.shortcuts import redirect, get_object_or_404
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
-
+from django.views.decorators.http import require_POST, require_GET
 from django.contrib import messages
 
 from .models import Tweet
+
+
+@login_required
+def home_page(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            form = TweetForm()
+            messages.success(request, 'Tweet created!')
+    else:
+        form = TweetForm()
+    # TODO following too
+    tweets = Tweet.objects.filter(user=request.user)
+    context = {
+        'form': form,
+        'tweets': tweets,
+        'user': request.user.username
+    }
+    return render(request, 'feed.html', context)
+
+
+@require_GET
+def user_profile(request, username):
+    user = get_object_or_404(get_user_model(), username=username)
+    tweets = Tweet.objects.filter(user=user)
+    return render(request, 'feed.html', {'tweets': tweets, 'user': username, 'other_user': username})
+
+
+@login_required
+@require_POST
+def follow(request):
+    pass
+
+
+@login_required
+@require_POST
+def unfollow(request):
+    pass
 
 
 @login_required()
@@ -15,6 +56,7 @@ def logout(request):
 
 
 @login_required()
+@require_POST
 def delete_tweet(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id)
     if tweet.user != request.user:
